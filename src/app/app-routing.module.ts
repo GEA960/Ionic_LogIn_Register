51e309310932174485299a76d186aa5f5ae30c7a
Gabriel19-00477 <gabrieledrian.alvaro@g.batstate-u.edu.ph>
2022-07-21 11:07:59 +0800

    Create a registration component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  },
 ];
 
 @NgModule({

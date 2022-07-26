62cf49a4322e6d6cd10a18791ba697bb4593b110
Gabriel19-00477 <gabrieledrian.alvaro@g.batstate-u.edu.ph>
2022-07-26 20:15:04 +0800

    Added Action Sheet Component

--- src/app/pages/homepage/homepage.page.ts
+++ src/app/pages/homepage/homepage.page.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController, ActionSheetController, AlertController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.page.html',
   styleUrls: ['./homepage.page.scss'],
 })
-export class HomepagePage implements OnInit {
+export class HomepagePage {
+  
+  
+  constructor(public actionSheetCtrl: ActionSheetController) { }
 
-  constructor() { }
+  async showActionSheet() {
+    await this.actionSheetCtrl.create({
+      header: 'Upload Photo',
+      buttons: [
+        {
+          text: "Take from Gallery",
+          handler: () => {
+            console.log("Gallery Clicked")
+          }
+        }, {
+          text: "Take Photo on Camera",
+          icon: 'camera',
+          handler: () => {
+            console.log("Camera Clicked")
+          }
+        },
 
-  ngOnInit() {
+        {
+          text: "Cancel",
+          role: "cancel",
+        }
+
+      ]
+    }).then(res => res.present());
+
   }
-
 }

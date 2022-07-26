7d6dc6db60b74c5445dcc88e001bc61c4695f26b
Gabriel19-00477 <gabrieledrian.alvaro@g.batstate-u.edu.ph>
2022-07-26 17:46:07 +0800

    re-route the sign in button into the homepage

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -29,7 +29,7 @@ export class LoginPage implements OnInit {
       });
       (await toast).present();
     }else{
-      this.router.navigate(['home'])
+      this.router.navigate(['homepage'])
     }
   }
 

7a067d11bf12c37c2653833568cf7b7b7e7929e0
Gabriel19-00477 <gabrieledrian.alvaro@g.batstate-u.edu.ph>
2022-07-26 14:10:52 +0800

    Created ToastController to display a warning if the fields were left blank after logging in

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController, ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  email: string = "";
+  password: string = "";
+
+  constructor(private router: Router, public toastCtrl: ToastController, public navCtrl: NavController, public loadingCtrl: LoadingController) { }
+
+  async login(){
+      if(this.email == ""){
+      const toast = this.toastCtrl.create({
+        message: 'Email cannot be empty',
+        duration: 3000
+      });
+      (await toast).present();
+    }else if(this.password==""){
+      const toast = this.toastCtrl.create({
+        message: 'Password cannot be empty',
+        duration: 3000
+      });
+      (await toast).present();
+    }else{
+      this.router.navigate(['home'])
+    }
+  }
+
+  register(){
+    this.router.navigate(['register'])
+  }
+
+  forgot(){
+    this.router.navigate(['forgot'])
+  }
 
   ngOnInit() {
   }
